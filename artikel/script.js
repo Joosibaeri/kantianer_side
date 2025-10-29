8e572fbc3047f6a1fe2d3143ce3ab87d1b5a24ed
Joosibaeri <joos.menken@icloud.com>
2025-10-29 19:50:34 +0100

    for future

---
+++ artikel/script.js
@@ -0,0 +1,8 @@
+// Setzt die Domain-Grundform in die Hitbox
+window.addEventListener('DOMContentLoaded', () => {
+    const domain = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ":" + window.location.port : "");
+    const link = document.getElementById('domain-link');
+    if (link) {
+        link.href = domain + "/";
+    }
+});
